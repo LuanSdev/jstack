c8ad71387fceb331989a8c1d8becfeaaa1c992a6
luanSdev <luang193@gmail.com>
2022-03-13 12:46:22 -0300

    feat: add user controller

---
+++ first-api/src/controllers/user-controller.js
@@ -0,0 +1,11 @@
+const users = require('../mocks/users');
+
+module.exports = {
+  listUsers(req, res) {
+    res.writeHead(200, {
+      'content-type': 'application/json',
+    });
+
+    res.end(JSON.stringify(users));
+  },
+};
